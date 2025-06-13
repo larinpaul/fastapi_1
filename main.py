84fa16032b403e3118ce4728c48b61aa661d0523
larinpaul <www.larinpavel@mail.ru>
2025-06-13 14:46:25 +0300

    # Some endpoints will do something CPU-backed # Doesn't make sense to make it async @api.get('/calculation') def calculation():     # do some heavy calculation     pass     return ""
    # But what if there is something like getting data from db... It depends on the kind of database request
    @api.get('/getdata')
    async def get_data_from_db():
        await request
        pass
        return ""
    
    # Some endpoints are going to be synchronous, and some - asynchronous...

--- main.py
+++ main.py
@@ -10,4 +10,19 @@ api = FastAPI()
 def index():
     return {"message": "Hello World"}
 
+# Some endpoints will do something CPU-backed # Doesn't make sense to make it async
+@api.get('/calculation')
+def calculation():
+    # do some heavy calculation
+    pass
+    return ""
+
+# But what if there is something like getting data from db... It depends on the kind of database request
+@api.get('/getdata')
+async def get_data_from_db():
+    await request
+    pass
+    return "" 
+
+# Some endpoints are going to be synchronous, and some - asynchronous...
 
